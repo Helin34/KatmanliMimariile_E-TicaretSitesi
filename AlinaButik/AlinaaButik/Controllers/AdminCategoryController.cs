346faa071fcae8960e302313d18c91d336e386ac
Helin Çelikbaş <helincelikbas142@gmail.com>
2022-09-25 05:41:29 +0300

    KatmanliMimariile_E-ticaretSitesi
    Katmanli Mimari ile E-Ticaret Sitesi

---
+++ AlinaButik/AlinaaButik/Controllers/AdminCategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlinaaButik.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        // GET: AdminCategory
+        CategoryRepository categoryRepository = new CategoryRepository();
+
+        public ActionResult Index()
+        {
+            return View(categoryRepository.List());
+        }
+    }
+}
---
+++ AlinaButik/AlinaaButik/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AlinaaButik.MvcApplication" Language="C#" %>
